75fb00670642d25ab19c6b667a12467167a51966
aditya <adityapathak63635@gmail.com>
2022-02-03 21:13:46 +0530

    edited

--- compoundint.sh
+++ compoundint.sh
@@ -2,7 +2,7 @@ echo "Enter the principle amount: "
 read p
 echo "Enter the rate of interest: "
 read r
-echo "Enter the no. of years: "
+echo "Enter the no. of year: "
 read t
 echo "Compound Interest is: "
 echo "scale=4; $p * ( 1 + $r / 100 ) ^ $t - $p" | bc
